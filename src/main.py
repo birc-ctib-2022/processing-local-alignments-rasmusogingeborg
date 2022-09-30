1109a3f90878e881c5043890535613a8095a3c82
RasmusUni <202004598@post.au.dk>
2022-09-30 10:21:39 +0200

    hej4

--- src/main.py
+++ src/main.py
@@ -141,11 +141,12 @@ if __name__ == '__main__':
         case 3:
             # one file argument
             print("Feature not implemented.", file=sys.stderr)
-            sys.exit(1)
+            infile = open(sys.argv[2],"r")
         case 4:
             # two file arguments
             print("Feature not implemented.", file=sys.stderr)
-            sys.exit(1)
+            infile = open(sys.argv[2], "r")
+            outfile = open(sys.argv[3], "w")
         case _:
             # either too few or too many arguments
             print("Incorrect number of arguments.", file=sys.stderr)
